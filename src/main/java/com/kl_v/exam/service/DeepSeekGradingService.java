0e750df4c7fc5609a56bfa335dacb8ef08e521b4
kyn <2077227081@qq.com>
2026-05-01 19:26:18 +0800

    初步deepseek化

---
+++ src/main/java/com/kl_v/exam/service/DeepSeekGradingService.java
@@ -0,0 +1,14 @@
+package com.kl_v.exam.service;
+
+/**
+ * ClassName: DeepSeekGradingService
+ * Package: com.kl_v.exam.service
+ * Description:
+ *
+ * @Author V
+ * @Create 2026/5/1 下午6:40
+ * @Version 1.0
+ */
+
+public interface DeepSeekGradingService {
+}
---
+++ src/main/java/com/kl_v/exam/service/impl/DeepSeekGradingService.java
@@ -0,0 +1,14 @@
+package com.kl_v.exam.service.impl;
+
+/**
+ * ClassName: deepSeekGradingService
+ * Package: com.kl_v.exam.service.impl
+ * Description:
+ *
+ * @Author V
+ * @Create 2026/5/1 下午6:40
+ * @Version 1.0
+ */
+
+public class DeepSeekGradingService implements com.kl_v.exam.service.DeepSeekGradingService {
+}
